8ed4d640812d0b85f2f67e83aac46c0a91499224
Marco Whyte <mwhyte@umass.edu>
2020-07-21 14:41:57 -0500

    mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,41 +2,50 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+
+        # place to keep track of variables to run your code
+        self.ram = [0] * 256
+        self.reg = [0] * 8  # like variables you have at your disposal
+        self.pc = 0  # where a computer is in its program sequence.
+
+        self.func_dict = {
+            1: self.HLT,
+            130: self.LDI,
+            71: self.PRN,
+        }
 
     def load(self):
         """Load a program into memory."""
 
         address = 0
-
         # For now, we've just hardcoded a program:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +57,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +71,34 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        self.status = True
+        while self.status == True:
+
+            # setting the instruction register to begin from the first memory
+            # copy of the currently executing instruction
+            ir = self.ram_read(self.pc)
+            opa = self.ram_read(self.pc + 1)  # Operation Code
+            opb = self.ram_read(self.pc + 2)
+
+            self.func_dict[ir](opa, opb)
+
+    def ram_read(self, address):
+        # Memory Data Register (MDR) = Memory Address Register (MAR)
+        self.content = self.ram[address]
+        return self.content
+
+    def ram_write(self, address, data):
+        self.ram[address] = data
+        self.ram_read(address)
+
+    def HLT(self, a, b):
+        self.status = False
+
+    def LDI(self, a, b):
+        self.reg[a] = b
+        self.pc += 3
+
+    def PRN(self, a, b):
+        value = self.reg[a]
+        print(value)
+        self.pc += 2
